4b9a963e70b837f71c1d7613de4915e9d5c53af4
EmmanuelN-L <46698046+EmmanuelN-L@users.noreply.github.com>
2021-02-24 23:26:50 -0500

    Added Lock, and rotation for screwdriver and lock based on input

---
+++ Assets/Scripts/Screwdriver_Rotation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Screwdriver_Rotation : MonoBehaviour
+{
+    public GameObject Pivot;
+
+    float smooth = 2.0f;
+    float tiltAngle = 90.0f;
+
+    float Rotator = 0.0f;
+    private void Update()
+    {
+        if (Input.GetKeyDown("a"))
+        {
+            Rotator = -1;
+        }
+
+        if (Input.GetKeyDown("d"))
+        {
+            Rotator = 0;
+        }
+
+        // Smoothly tilts a transform towards a target rotation.
+        float tiltAroundZ = Rotator * tiltAngle;
+
+        // Rotate the cube by converting the angles into a quaternion.
+        Quaternion target = Quaternion.Euler(0, 0, tiltAroundZ);
+
+        // Dampen towards the target rotation
+        transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
+    }
+}
